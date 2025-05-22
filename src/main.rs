3ba907f65a03d9096f97644ff743d144b0416a7a
Hugh Brown <hughdbrown@yahoo.com>
2025-05-22 15:17:11 -0600

    Add tokio tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use anyhow::{self, Context};
 use reqwest;
 use tokio;
 
-async fn fetch_data(url: &str) -> anyhow::Result<String, anyhow::Error>
+type AnyhowError = anyhow::Result<String, anyhow::Error>;
+
+async fn fetch_data(url: &str) -> AnyhowError
 {
     let response = reqwest::get(url).await.context("Failed to fetch data")?;
     let text = response.text().await.context("Failed to parse response")?;
@@ -16,5 +18,27 @@ async fn main() {
     match result {
          Ok(text) => println!("{}", text),
          Err(err) => println!("{}", err),
+    }
+}
+
+#[tokio::test]
+async fn test_fetch_data_error() {
+    let url = &"https://google.com/asdasd";
+    let result: AnyhowError = fetch_data(&url).await;
+
+    match result {
+        Ok(x) => println!("Not an error: {}", x),
+        Err(_) => assert!(result.is_err()),
+    }
+}
+
+#[tokio::test]
+async fn test_fetch_data_no_error() {
+    let url = &"https://google.com/";
+    let result: AnyhowError = fetch_data(&url).await;
+
+    match result {
+        Ok(_) => assert!(! result.is_err()),
+        Err(x) => println!("Unexpected error: {}", x),
     }
 }
